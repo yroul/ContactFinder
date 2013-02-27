b8a09e1ce1b07cb33250d6f651149aba00f0531c
USERCCI <usercci@Poste14.local>
2013-02-27 14:57:20 +0100

    Initial Commit

---
+++ ContactFinder/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ContactFinder
+//
+//  Created by USERCCI on 27/02/13.
+//  Copyright (c) 2013 yroul. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ContactFinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContactFinder
+//
+//  Created by USERCCI on 27/02/13.
+//  Copyright (c) 2013 yroul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
